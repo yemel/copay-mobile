20d03d819a23b49b522c8968f4ec4a945dabf673
Yemel Jardi <angel.jardi@gmail.com>
2014-09-22 08:49:06 -0300

    First commit
    Create project structure and basic views.

---
+++ www/js/app.js
@@ -0,0 +1,117 @@
+// Copay App
+
+angular.module('copay', ['ionic', 'copay.controllers'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      // org.apache.cordsova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+    .state('welcome', {
+      url: "/",
+      templateUrl: "templates/welcome.html",
+      // controller: 'WelcomeCtrl'
+    })
+
+    .state('settings', {
+      url: "/settings",
+      templateUrl: "templates/settings.html",
+      // controller: 'AppCtrl'
+    })
+
+    .state('register', {
+      url: "/register",
+      templateUrl: "templates/register.html",
+      controller: 'RegisterCtrl'
+    })
+
+    .state('login', {
+      url: "/login",
+      templateUrl: "templates/login.html",
+      controller: 'RegisterCtrl'
+    })
+
+    .state('setPin', {
+      templateUrl: "templates/pin.html",
+      controller: 'SetPinCtrl'
+    })
+
+    .state('confirmPin', {
+      templateUrl: "templates/pin.html",
+      controller: 'ConfirmPinCtrl'
+    })
+
+    .state('wallet', {
+      abstract: true,
+      templateUrl: "templates/menu.html",
+    })
+
+    .state('wallet.home', {
+      url: "/wallet/home",
+      templateUrl: "templates/menu.html",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/home.html"
+        }
+      }
+    })
+
+    // .state('profile.profile', {
+    //   url: "/app",
+    //   abstract: true,
+    //   templateUrl: "templates/menu.html",
+    //   controller: 'AppCtrl'
+    // })
+
+    // .state('profile.search', {
+    //   url: "/search",
+    //   views: {
+    //     'menuContent' :{
+    //       templateUrl: "templates/search.html"
+    //     }
+    //   }
+    // })
+
+    // .state('app.profile.browse', {
+    //   url: "/browse",
+    //   views: {
+    //     'menuContent' :{
+    //       templateUrl: "templates/browse.html"
+    //     }
+    //   }
+    // })
+    // .state('app.profile.playlists', {
+    //   url: "/playlists",
+    //   views: {
+    //     'menuContent' :{
+    //       templateUrl: "templates/playlists.html",
+    //       controller: 'PlaylistsCtrl'
+    //     }
+    //   }
+    // })
+
+    // .state('app.profile.single', {
+    //   url: "/playlists/:playlistId",
+    //   views: {
+    //     'menuContent' :{
+    //       templateUrl: "templates/playlist.html",
+    //       controller: 'PlaylistCtrl'
+    //     }
+    //   }
+    // });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/');
+});
+
---
+++ www/js/controllers.js
@@ -0,0 +1,39 @@
+angular.module('copay.controllers', [])
+
+.controller('RegisterCtrl', function($scope, $state, $ionicLoading, $timeout) {
+  $scope.submit = function() {
+    $ionicLoading.show({
+      template: '<i class="icon ion-loading-c"></i> Doing something...'
+    });
+
+    $timeout(function() {
+      $ionicLoading.hide();
+      $state.go('setPin');
+    }, 2000);
+
+  };
+})
+
+.controller('SetPinCtrl', function($scope, $state) {
+  $scope.message = "Enter a 4-digit pin";
+  $scope.digits = [];
+
+  $scope.press = function(digit) {
+    $scope.digits.push(digit);
+    if($scope.digits.length == 4) {
+      $state.go('confirmPin');
+    }
+  };
+})
+
+.controller('ConfirmPinCtrl', function($scope, $state) {
+  $scope.message = "Confirm your 4-digit pin";
+  $scope.digits = [];
+
+  $scope.press = function(digit) {
+    $scope.digits.push(digit);
+    if($scope.digits.length == 4) {
+      $state.go('wallet.home');
+    }
+  };
+})
