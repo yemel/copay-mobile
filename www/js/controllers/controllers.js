9208a726c81b57bfa12a55db51c740e73b2b3021
ssotomayor <sansoto2003@yahoo.com.ar>
2014-09-23 15:07:10 -0300

    login mock service

--- www/js/controllers/controllers.js
+++ www/js/controllers/controllers.js
@@ -20,6 +20,7 @@ angular.module('copay.controllers', [])
                     }
                 });
             }
+            $scope.usernameValidate = true;
             $ionicLoading.hide();
         };
     })
--- www/js/services/services.js
+++ www/js/services/services.js
@@ -3,13 +3,11 @@ angular.module('copay.services', [])
     .factory('DataSrv', function ($ionicLoading, $timeout) {
         return {
             loginToApp: function (username, pwd, callback) {
-                var data,
+                var data = null,
                     err  = null;
 
                 if (username === 'a' && pwd === 'a') {
                     data = {name: "John", lastname: "Doe", birthdate: '04/04/04'};
-                } else {
-                    data = null;
                 }
 
                 //err = { message: "Service Failed" };
