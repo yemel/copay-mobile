178144e4cf6c0f466fc53e35d742451db41ca6a9
Yemel Jardi <angel.jardi@gmail.com>
2014-10-15 17:46:23 -0300

    Add QR modal

--- www/js/controllers/controllers.js
+++ www/js/controllers/controllers.js
@@ -177,9 +177,24 @@ angular.module('copay.controllers', [])
   });
 })
 
-.controller('HomeCtrl', function($scope, $state) {
+.controller('HomeCtrl', function($scope, $state, $ionicModal) {
   $scope.copayers = $scope.wallet.getRegisteredPeerIds(); // TODO: Rename method to getCopayers
   $scope.remaining = $scope.wallet.publicKeyRing.remainingCopayers(); // TODO: Expose on Wallet
+
+  $ionicModal.fromTemplateUrl('templates/qr.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  $scope.openModal = function() {
+    $scope.modal.show();
+  };
+
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  };
 })
 
 .controller('ReceiveCtrl', function($scope, $state) {
