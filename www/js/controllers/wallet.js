d064fa282f078f27926b36c2ecbcb5714691fd53
Esteban Ordano <eordano@gmail.com>
2014-10-23 19:08:32 -0300

    Show a popup if the provided secret is invalid

--- www/js/controllers/wallet.js
+++ www/js/controllers/wallet.js
@@ -2,7 +2,7 @@
 
 angular.module('copay.controllers')
 
-.controller('WalletCtrl', function($scope, $state, $ionicLoading, Wallets) {
+.controller('WalletCtrl', function($scope, $state, $ionicLoading, $ionicPopup, Wallets) {
   // Current limitations of multisig and transaction size
   var COPAYERS_LIMIT = 12;
   var THRESHOLD_LIMITS = [1, 2, 3, 4, 4, 3, 3, 2, 2, 2, 1, 1];
@@ -34,7 +34,20 @@ angular.module('copay.controllers')
     console.log('SECRET', $scope.data.secret);
     Wallets.join($scope.data.secret, function onResult(err, wallet){
       $ionicLoading.hide();
-      if (err) throw err;
+      if (err) {
+        if (err === Wallets.error.badSecret) {
+          var alertPopup = $ionicPopup.alert({
+            title: 'Invalid secret',
+            template: 'The provided secret is invalid. Please check it and try again.'
+          });
+          alertPopup.then(function(res) {
+            $scope.data.secret = '';
+          });
+          return;
+        } else {
+          throw err;
+        }
+      }
       return $state.go('profile.wallet.home', {walletId: wallet.id});
     });
   };
